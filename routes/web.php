5481ea1b113a3aebaecead96522c574ff9caec61
Simon Garrick <simon.garrick@jaywing.com>
2022-10-17 16:00:17 +0100

    laravel tutorial for CRUD chirps

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ChirpController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('chirps', ChirpController::class)
+->only(['index', 'store', 'edit', 'update', 'destroy'])->middleware(['auth', 'verified']);
+
+
+require __DIR__.'/auth.php';
