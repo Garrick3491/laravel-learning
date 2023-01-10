121404b41e57d18afb7c22155a50c34839f2b83d
Simon Garrick <simon.garrick@Parallaxs-MBP.parall.ax>
2023-01-10 14:35:46 +0000

    purple colours, smaller buttons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="max-w-7xl p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <div class="p-6 bg-white border-b border-purple-200">
+                    <div class="max-w-7xl p-6 bg-white border border-purple-200 rounded-lg">
                             <form action="{{route('device-upload')}}" method="post" enctype="multipart/form-data">
                                 @csrf
                                 @if ($message = Session::get('success'))
@@ -32,25 +32,25 @@
                                 </div>
                                 @endif
                                 <div class="custom-file">
-                                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-                                    <input name="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+                                    <label class="block mb-2 text-sm font-medium" for="file_input">Upload file</label>
+                                    <input name="file" class="block w-full text-sm border border-purple-300 rounded-lg cursor-pointer focus:outline-none" id="file_input" type="file">
                                 </div>
                             <br>
-                            <button type="submit" name="submit" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            <button type="submit" name="submit" class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                 Upload File
                             </button>
                         </form>
                     </div>
                     <br>
                     @foreach ($devices as $device)
-                    <div class="max-w-7xl p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <div class="max-w-7xl p-6 bg-white border border-purple-200 rounded-lg">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">
                                 {{ $device->name }}
                         </h5>
                         <br>
-                        <a class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="{{route('view-device', $device->id)}}">View Device</a>
-                        <a class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"  href="{{route('update-device-form', $device->id)}}">Edit device</a>
-                        <a class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="{{route('delete-device', $device->id)}}" onclick="return confirm('Are you sure?')">Delete device</a>
+                        <a class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" href="{{route('view-device', $device->id)}}">View Device</a>
+                        <a class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"  href="{{route('update-device-form', $device->id)}}">Edit device</a>
+                        <a class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" href="{{route('delete-device', $device->id)}}" onclick="return confirm('Are you sure?')">Delete device</a>
                     </div>
                     <br>
                     @endforeach
@@ -58,9 +58,9 @@
                         @foreach ($links as $link)
                             <li>
                                 @if($link->active == true)
-                                    <a href="{{$link->url}}" aria-current="page" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">{{ $link->label }}</a>
+                                    <a href="{{$link->url}}" aria-current="page" class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{{ $link->label }}</a>
                                 @else
-                                    <a href="{{$link->url}}" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">{!!$link->label!!}</a>
+                                    <a href="{{$link->url}}" class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{!!$link->label!!}</a>
                                 @endif
                             </li>
                         @endforeach
--- resources/views/view-device.blade.php
+++ resources/views/view-device.blade.php
@@ -8,21 +8,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="max-w-7xl p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <div class="p-6 bg-white border-b border-purple-200">
+                    <div class="max-w-7xl p-6 bg-white border border-purple-200 rounded-lg shadow-md">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight">
                                 {{ $device->name }}
                         </h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Address: {{$device->address}}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Geographic Coordinates: {{$device->latitude}}, {{$device->longitude}}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Device: {{ $device->model}} - {{ $device->device_type }} - {{$device->manufacturer}}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Install Date: {{$device->install_date}}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Notes: {{$device->notes}}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">EUI: {{$device->eui}}</p>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Serial Number: {{$device->serial_number}}</p>
+                        <p class="mb-3 font-normal">Address: {{$device->address}}</p>
+                        <p class="mb-3 font-normal">Geographic Coordinates: {{$device->latitude}}, {{$device->longitude}}</p>
+                        <p class="mb-3 font-normal">Device: {{ $device->model}} - {{ $device->device_type }} - {{$device->manufacturer}}</p>
+                        <p class="mb-3 font-normal">Install Date: {{$device->install_date}}</p>
+                        <p class="mb-3 font-normal">Notes: {{$device->notes}}</p>
+                        <p class="mb-3 font-normal">EUI: {{$device->eui}}</p>
+                        <p class="mb-3 font-normal">Serial Number: {{$device->serial_number}}</p>
                         <br><br>
-                        <a class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"  href="{{route('update-device-form', $device->id)}}">Edit device</a>
-                        <a class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" href="{{route('delete-device', $device->id)}}" onclick="return confirm('Are you sure?')">Delete device</a>
+                        <a class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"  href="{{route('update-device-form', $device->id)}}">Edit device</a>
+                        <a class="px-3 py-2 text-xs font-medium text-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" href="{{route('delete-device', $device->id)}}" onclick="return confirm('Are you sure?')">Delete device</a>
                     </div>
                 </div>    
             </div>
